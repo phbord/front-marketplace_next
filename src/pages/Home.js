7dfc6406b219d73a5ecf193c71c48becc32d5511
phbord <phbord@gmail.com>
2021-09-06 17:14:56 +0200

    modify home and header

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,34 @@
 import React from 'react'
-
-import data from '../data/dumbdata.json'
+import { Layout } from 'antd';
 import { Card, Col, Row } from 'antd';
 import { v4 as uuid_v4 } from "uuid";
+
+import data from '../data/dumbdata.json';
+
+const { Content } = Layout;
+
 export const Home = () => {
 
 	const { flats } = data
 	const { Meta } = Card;
 	return (
-		<div>
-			<Row gutter={16}>
-				{flats.map(flat => (
-					<>
-					<Col span={{xs: 24, sm: 12, md: 8, lg: 6}}>
-						<Card
-							key={uuid_v4()}
-							hoverable
-							style={{ border:"1px solid black" }}
-							cover={<img alt="example" src={flat.images[0]} />}
-						>
-							<Meta title={flat.name}  />
-						</Card>
-					</Col>
-					</>
-				))}
-			</Row>
-		</div>)
+		<>
+			<div className="home-header">
+				<h1>List of real estate</h1>
+			</div>
+			<Content style={{ padding: '0 50px' }}>
+				<Row gutter={16}>
+					{flats.map(flat => (
+						<Col key={uuid_v4()} span={{xs: 24, sm: 12, md: 8, lg: 6}}>
+							<Card
+								hoverable
+								style={{ border:"1px solid black" }}
+								cover={<img alt="example" src={flat.images[0]} />}>
+								<Meta title={flat.name}  />
+							</Card>
+						</Col>
+					))}
+				</Row>
+			</Content>
+		</>)
 }
