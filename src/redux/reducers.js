09e78d414e54f683043cde20aa047f5107daf34c
phbord <phbord@gmail.com>
2021-09-07 16:42:15 +0200

    create an real estate

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
 import realestatesReducer from 'redux/realestates/realestatesReducer'
+import realestateReducer from 'redux/realestate/realestateReducer'
 
 export default combineReducers({
   realestates: realestatesReducer,
+  realestate: realestateReducer,
 });
