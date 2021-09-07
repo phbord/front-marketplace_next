11d97aa1f92c70d32a2cce979b57ca1b5337ac26
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-09-08 00:50:56 +0200

    done, but issue with inputs/bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react'
-import { Layout, Menu } from "antd";
+import { Layout } from "antd";
 import { Link, useHistory } from "react-router-dom";
 import Cookies from "js-cookie";
 import { useDispatch } from "react-redux";
@@ -19,17 +18,44 @@ export const Navbar = () => {
     history.push('/');
   }
 
-
-
-
 	return (
-		<nav className="navbar">
-      <h1>THPGram</h1>
-      <Link to="/">Home</Link>
-      {cookie && <Link to="/profile">Profile</Link>}
-      {cookie && <Link to="#" onClick={e => logMeOut(e)}>Logout</Link>}
-      {!cookie && <Link to="/login">Login</Link>}
-      {!cookie && <Link to="/register">Register</Link>}
-    </nav>
-	)
+  <>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+      <div class="row align-items-start">
+          <a class="navbar-brand" ><Link className="title" to="/">THPGram</Link></a>
+      </div>
+      <div class="row align-items-center" name="search-input">
+      { /*<form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> 
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form> */ }
+      </div>
+      <div class="row align-items-end" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" >{!cookie && <Link className="menu-links"to="/login">Login</Link>}</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" >{!cookie && <Link className="menu-links" to="/register">Register</Link>}</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" >{cookie && <Link className="menu-links" to="#" onClick={e => logMeOut(e)}>Logout</Link>}</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" >{cookie && <Link className="menu-links" to="/profile">Profile</Link>}</a>
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+	</>
+  )
 }
+
+
+
