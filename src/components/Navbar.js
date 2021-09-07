494a91f184635d764657566e18be2a75fbb9382b
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-09-07 16:59:36 +0200

    navbar remade

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,35 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { Layout, Menu } from "antd";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import Cookies from "js-cookie";
+import { useDispatch } from "react-redux";
+import { logout } from "redux/auth/authActions";
 
 const { Header } = Layout;
 
 export const Navbar = () => {
+
+	const history = useHistory();
+  const cookie = Cookies.get('token');
+  const dispatch = useDispatch();
+
+  const logMeOut = (e) => {
+    e.preventDefault();
+    dispatch(logout());
+    history.push('/');
+  }
+
+
+
+
 	return (
-		<Header style={{ position: "fixed", zIndex: 1, width: "100%", display: "flex" }}>
-			<h1 className="logo">Real Estate</h1>
-			<Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-				<Menu.Item key="1">
-					<Link to="/">Home</Link>
-				</Menu.Item>
-				<Menu.Item key="2">
-					<Link to="/other">other</Link>
-				</Menu.Item>
-				<Menu.Item key="3">
-					<Link to="/login">login</Link>
-				</Menu.Item>
-				<Menu.Item key="4">
-					<Link to="/register">register</Link>
-				</Menu.Item>
-			</Menu>
-		</Header>
+		<nav className="navbar">
+      <h1>THPGram</h1>
+      <Link to="/">Home</Link>
+      {cookie && <Link to="/profile">Profile</Link>}
+      {cookie && <Link to="#" onClick={e => logMeOut(e)}>Logout</Link>}
+      {!cookie && <Link to="/login">Login</Link>}
+      {!cookie && <Link to="/register">Register</Link>}
+    </nav>
 	)
 }
