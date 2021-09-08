43030b6553afdff2a4fbf5d16fb8b8d99b939c6b
Septentrion50 <erwan56lecorre@gmail.com>
2021-09-08 14:00:26 +0200

    Changes to redux and images in RealEstate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { useSelector } from 'react-redux'
-import { Layout, Card } from 'antd';
 import { v4 as uuid_v4 } from "uuid";
-import { getListing } from 'Utils/fetchFunctions';
-
-const cookie = Cookies.get('token');
 
 export const Home = () => {
 	const [flats, setFlats] = React.useState([]);
 
 	React.useEffect(() => {
-		setFlats(getListing('real_estates'))
-		console.log(flats)
+		getListing('real_estates')
 	}, [])
+
+	const getListing = async(url) => {
+		const config = {
+			method: 'GET',
+		};
+
+		const response = await fetch(`${process.env.REACT_APP_API_URL}/${url}`, config);
+		const data = await response.json();
+		console.log(data)
+		setFlats(data)
+	};
 
 	return (
 		<>
@@ -22,25 +27,26 @@ export const Home = () => {
 					<h1>List of real estates</h1>
 				</div>
 			</div>
-			<Content style={{ padding: '0 3.5rem' }}>
-				<Row gutter={16}>
-					{flats ? ( flats.map(flat => (
-						<Col span={{xs: 24, sm: 12, md: 8, lg: 6}} key={uuid_v4()}>
-							<Link to={`/real_estates/${flat.id}`} className="card-link">
-								<Card hoverable
-											style={{ border:"1px solid black" }}
-											title={flat.title}
-											cover={<img alt="example" src={flat.id} />}>
-									<p>Price : {flat.price} €</p>
-									<p>location : {flat.location}</p>
-								</Card>
-							</Link>
-						</Col>
-					))
-					):(
+			<div className="container" style={{ padding: '0 3.5rem' }}>
+				<ul className="row">
+					{flats ? (
+						flats.map(flat => (
+							<li className="col-12 col-sm-6 col-md-4 col-lg-3" key={uuid_v4()}>
+								<Link to={`/real_estates/${flat.id}`} className="card-link">
+									<div className="card h-100">
+										<div className="card-body">
+											<h5>{flat.title}</h5>
+											<p>Price : {flat.price} €</p>
+											<p>location : {flat.location}</p>
+										</div>
+									</div>
+								</Link>
+							</li>
+						))
+					) : (
 						<h2>Loading...</h2>
 					)}
-				</Row>
-			</Content>
+				</ul>
+			</div>
 		</>)
 }
--- src/pages/Realestate.js
+++ src/pages/Realestate.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { useParams } from "react-router-dom";
-import { Carousel, Layout, Image, Space, Col, Row } from 'antd';
-import { getListing } from 'Utils/fetchFunctions';
+import { Carousel, Image } from 'antd';
 
 export const Realestate = () => {
   const { id } = useParams()
-  const [listing, setListing] = React.useState({})
+  const [listing, setListing] = React.useState('') || {}
 
-	React.useEffect(() => {
-    setListing(getListing(`real_estates/${id}`))
-	}, [listing])
+  React.useEffect(() => {
+    getListing(`real_estates/${id}`)
+    console.log('listing:', listing)
+  }, [])
+
+  const getListing = async (url) => {
+    const config = {
+      method: 'GET',
+    };
+
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/${url}`, config);
+    const data = await response.json();
+    console.log(data)
+    setListing(data)
+  };
 
 
   return (<>
@@ -18,27 +29,27 @@ export const Realestate = () => {
         <Carousel autoplay style={{ padding: '4rem 0 3rem' }}>
           <div className="realestate-carousel">
             <div>
-              <Image className="realestate-carousel-image" 
-                      preview={{ visible: false }}
-                      src={ `${process.env.REACT_APP_API_URL}${listing?.images_url[0]}` || "https://images.pexels.com/photos/269077/pexels-photo-269077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"} />
+              <Image className="realestate-carousel-image"
+                preview={{ visible: false }}
+                src={`${process.env.REACT_APP_API_URL}${listing.images_url[0]}` || "https://images.pexels.com/photos/269077/pexels-photo-269077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"} />
             </div>
           </div>
           <div className="realestate-carousel">
             <div>
-              <Image className="realestate-carousel-image" 
-                      preview={{ visible: false }}
-                      src="https://images.pexels.com/photos/1059078/pexels-photo-1059078.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+              <Image className="realestate-carousel-image"
+                preview={{ visible: false }}
+                src={`${process.env.REACT_APP_API_URL}${listing.images_url[1]}` || "https://images.pexels.com/photos/1059078/pexels-photo-1059078.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"} />
             </div>
           </div>
         </Carousel>
-        <Content style={{ padding: '0 3.5rem 3rem' }}>
+        <div className="container" style={{ padding: '0 3.5rem 3rem' }}>
           <h1>{listing.title}</h1>
           <p>Location: {listing.location}</p>
           <p>Price: <strong>{listing.price}</strong></p>
           <p>Address: {listing.address}</p>
           <p>{listing.description}</p>
-          { <p>Owner: {listing?.user?.email}</p> }
-        </Content>
+          {<p>Owner: {listing?.user?.email}</p>}
+        </div>
       </>
     ) : (
       <h2>Loading...</h2>
