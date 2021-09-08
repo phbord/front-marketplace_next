8e8ec5108cbe8fc2e9622b5985e71582f974db13
phbord <phbord@gmail.com>
2021-09-08 19:54:01 +0200

    new real estate finish

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
 import { v4 as uuid_v4 } from "uuid";
 
 export const Home = () => {
+	const connected = useSelector(state => state.auth.user)
 	const [flats, setFlats] = React.useState([]);
 
 	React.useEffect(() => {
 		getListing('real_estates')
 	}, [])
 
-	const getListing = async(url) => {
+	const getListing = async (url) => {
 		const config = {
 			method: 'GET',
 		};
 
 		const response = await fetch(`${process.env.REACT_APP_API_URL}/${url}`, config);
 		const data = await response.json();
-		console.log(data)
+		
 		setFlats(data)
 	};
 
@@ -32,16 +34,15 @@ export const Home = () => {
 					{flats ? (
 						flats.map(flat => (
 							<li className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4" key={uuid_v4()}>
-								<Link to={`/real_estates/${flat.id}`} className="card-link">
-									<div className="card h-100">
-										{ flat.images_url && <img src={`${process.env.REACT_APP_API_URL}${flat.images_url[0]}`} className="card-img-top" alt=""/> }
-										<div className="card-body">
-											<h5>{flat.title}</h5>
-											<p>Price : {flat.price} €</p>
-											<p>location : {flat.location}</p>
-										</div>
+								<div className="card h-100">
+									{ flat.images_url && <img src={`${process.env.REACT_APP_API_URL}${flat.images_url[0]}`} className="card-img-top" alt=""/> }
+									<div className="card-body">
+										<h5>{flat.title}</h5>
+										<p>Price : {flat.price} €</p>
+										<p>location : {flat.location}</p>
+										{connected && <Link to={`/real_estates/${flat.id}`} className="btn btn-primary">Details</Link>}
 									</div>
-								</Link>
+								</div>
 							</li>
 						))
 					) : (
--- src/pages/NewRealestate.js
+++ src/pages/NewRealestate.js
@@ -1,17 +1,72 @@
 import React from 'react';
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { v4 as uuid_v4 } from "uuid";
+import Cookies from 'js-cookie';
+
 import { register } from "redux/auth/authActions";
 
 const NewRealestate = () => {
   const history = useHistory()
+	const [categories, setCategories] = React.useState([])
+	const [selectValue, setSelectValue] = React.useState('')
+	const [categoryValue, setCategoryValue] = React.useState('')
 
-  const createNewRealestate = e => {
+  const selectEl = React.useRef(null)
+  const titleEl = React.useRef(null)
+  const descriptionEl = React.useRef(null)
+  const locationEl = React.useRef(null)
+  const addressEl = React.useRef(null)
+  const priceEl = React.useRef(null)
+
+  const createNewRealestate = async e => {
     e.preventDefault();
-    const creds = {};
-    //dispatch(register(creds));
+    
+    let token = ''
+
+    const creds = {
+      title: titleEl.current.value,
+      description: descriptionEl.current.value,
+      location: locationEl.current.value,
+      category: selectValue ? selectValue : categoryValue,
+      address: addressEl.current.value,
+      price: priceEl.current.value,
+    };
+    console.log('CREDS => ',creds);
+
+    const config = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${Cookies.get('token')}`
+      },
+      body: JSON.stringify(creds)
+    }
+    
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/real_estates`, config);
+    
     history.push('/');
   }
+
+  const getCategories = async (url) => {
+    const config = {
+			method: 'GET',
+		};
+
+		const response = await fetch(`${process.env.REACT_APP_API_URL}/${url}`, config);
+		const data = await response.json();
+		
+    setCategories(data)
+    setCategoryValue(data[0].title)
+  }
+
+  const handleChange = () => {
+    setSelectValue(selectEl.current.value)
+  }
+
+  React.useEffect(() => {
+    getCategories('categories')
+  }, [])
 
   return (
     <>
@@ -19,24 +74,34 @@ const NewRealestate = () => {
         <h1 className="mt-5">New real estate</h1>
         <form onSubmit={e => createNewRealestate(e)}>
           <div className="mb-3">
-            <label htmlFor="title" class="form-label">title</label>
-            <input type="text" className="form-control" id="title" />
+            <label htmlFor="title" className="form-label">Title</label>
+            <input type="text" className="form-control" id="title" ref={titleEl}/>
           </div>
           <div className="mb-3">
-            <label htmlFor="description" class="form-label">description</label>
-            <textarea className="form-control" id="description" />
+            <label htmlFor="description" className="form-label">Description</label>
+            <textarea className="form-control" id="description" ref={descriptionEl}/>
           </div>
           <div className="mb-3">
-            <label htmlFor="location" class="form-label">location</label>
-            <input type="text" className="form-control" id="location" />
+            <label htmlFor="location" className="form-label">Location</label>
+            <input type="text" className="form-control" id="location" ref={locationEl}/>
           </div>
           <div className="mb-3">
-            <label htmlFor="address" class="form-label">address</label>
-            <input type="text" className="form-control" id="address" />
+            <label htmlFor="category" className="form-label">Category</label>
+            <select className="form-select" id="category" ref={selectEl} onChange={handleChange}>
+              {
+                categories.map((cat, index) => (
+                  <option key={uuid_v4()}>{cat.title}</option>
+                ))
+              }
+            </select>
           </div>
           <div className="mb-3">
-            <label htmlFor="price" class="form-label">price</label>
-            <input type="text" className="form-control" id="price" />
+            <label htmlFor="address" className="form-label">Address</label>
+            <input type="text" className="form-control" id="address" ref={addressEl}/>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="price" className="form-label">Price</label>
+            <input type="text" className="form-control" id="price" ref={priceEl}/>
           </div>
           <div className="mb-3">
             <button type="submit" className="btn btn-primary">Submit</button>
@@ -49,7 +114,5 @@ const NewRealestate = () => {
 
 export default NewRealestate;
 
-//     t.integer "user_id"
-//     t.integer "category_id"
-//     t.index ["category_id"], name: "index_real_estates_on_category_id"
-//     t.index ["user_id"], name: "index_real_estates_on_user_id"
+
+// "category_id" > select avec fetch get sur cat + renvoi du nom de la cat
