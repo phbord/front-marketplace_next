2a4db386954cbd4f6b86ac800e49341b4d2186d3
Septentrion50 <erwan56lecorre@gmail.com>
2021-09-09 13:57:01 +0200

    Replace the multiple images field by 3 fields

--- src/pages/NewRealestate.js
+++ src/pages/NewRealestate.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { v4 as uuid_v4 } from "uuid";
 import Cookies from 'js-cookie';
-
-import { register } from "redux/auth/authActions";
 
 const NewRealestate = () => {
   const history = useHistory()
@@ -18,22 +15,23 @@ const NewRealestate = () => {
   const locationEl = React.useRef(null)
   const addressEl = React.useRef(null)
   const priceEl = React.useRef(null)
-  const images = React.useRef(null)
+  const image1 = React.useRef(null)
+  const image2 = React.useRef(null)
+  const image3 = React.useRef(null)
 
   const createNewRealestate = async e => {
     e.preventDefault();
-
-    let token = ''
-
     let formData = new FormData();
 
     formData.append('title', titleEl.current.value)
     formData.append('description', descriptionEl.current.value)
     formData.append('location', locationEl.current.value)
-    formData.append('category', selectValue ? selectValue : categoryValue)
+    formData.append('category', selectValue ? selectValue : categories.filter(c => c.title === categoryValue)[0].id)
     formData.append('address', addressEl.current.value)
     formData.append('price', priceEl.current.value)
-    formData.append('images', images.current.files)
+    formData.append('image1', image1.current.files[0])
+    formData.append('image2', image2.current.files[0])
+    formData.append('image3', image3.current.files[0])
 
     const config = {
       method: 'POST',
@@ -43,9 +41,8 @@ const NewRealestate = () => {
       },
       body: formData
     }
-
     const res = await fetch(`${process.env.REACT_APP_API_URL}/real_estates`, config);
-
+    console.log(res)
     history.push('/');
   }
 
@@ -53,10 +50,8 @@ const NewRealestate = () => {
     const config = {
       method: 'GET',
     };
-
     const response = await fetch(`${process.env.REACT_APP_API_URL}/${url}`, config);
     const data = await response.json();
-
     setCategories(data)
     setCategoryValue(data[0].title)
   }
@@ -105,8 +100,16 @@ const NewRealestate = () => {
             <input type="text" className="form-control" id="price" ref={priceEl} />
           </div>
           <div className="mb-3">
-            <label htmlFor="images" className="form-label">Images</label>
-            <input type="file" className="form-control" id="images" multiple accept="image/*" ref={images} />
+            <label htmlFor="image1" className="form-label">Image 1</label>
+            <input type="file" className="form-control" id="image1" required accept="image/*" ref={image1} />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="image2" className="form-label">Image 2</label>
+            <input type="file" className="form-control" id="image2" accept="image/*" ref={image2} />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="image3" className="form-label">Image 3</label>
+            <input type="file" className="form-control" id="image3" accept="image/*" ref={image3} />
           </div>
           <div className="mb-3">
             <button type="submit" className="btn btn-primary">Submit</button>
