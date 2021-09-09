be800ef3f71d281f4f97f94b1069003b8b8d3b60
Septentrion50 <erwan56lecorre@gmail.com>
2021-09-09 17:08:21 +0200

    Updates

--- src/services/api/index.js
+++
@@ -1,7 +0,0 @@
-// FOR PROD USE
-export const API_LOCATION = "https://api-marketplace-next-prod.herokuapp.com/"
-export const baseName = "/front-marketplace_next"
-
-// FOR DEV USE
-// export const API_LOCATION = "http://localhost:3000"
-// export const baseName = "/"
