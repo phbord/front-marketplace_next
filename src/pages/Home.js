4f7833f4f73c3d0c0f8d3db6f24fe2e59659662d
phbord <phbord@gmail.com>
2021-09-09 20:14:08 +0200

    delete modification

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,7 +19,7 @@ export const Home = () => {
 			method: 'GET',
 		};
 
-		const response = await fetch(`${process.env.REACT_APP_API_URL}/${url}`, config);
+		const response = await fetch(`${process.env.REACT_APP_API_URL}${url}`, config);
 		const data = await response.json();
 		
 		setFlats(data)
