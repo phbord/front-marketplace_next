e59c10a65ef938c6fcb7e12748e4865949a23d88
phbord <phbord@gmail.com>
2021-09-09 13:01:55 +0200

    create update real estate page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,10 @@
 import Cookies from "js-cookie";
 import React from "react";
+import { Link } from "react-router-dom";
+import { v4 as uuid_v4 } from "uuid";
 
 const Profile = ({ user }) => {
-
   console.log('PROFILE USER: ', user)
-
   const [listing, setListing] = React.useState('') || []
 
   React.useEffect(() => {
@@ -31,29 +31,40 @@ const Profile = ({ user }) => {
       {user ? (
         <div className="container my-4">
           <h1>My Profile</h1>
-          <h3>{user.attributes?.email}</h3>
-          <h3>My Listings</h3>
-          <hr/>
-          {listing ? (
-            listing.map(l => (
-              <div className="listing-profile" key={l.id}>
-                <h4>{l.title}</h4>
-                <h4>{l.description}</h4>
-                <h4>{l.address}</h4>
-                <h4>{l.location}</h4>
-                <ul className="photos">
-                {l.images_url.map((image, index) => (
-                  <li key={index}>
-                    <img src={`${process.env.REACT_APP_API_URL}${image}`} />
-                  </li>
-                ))}
-                </ul>
-                <hr/>
-              </div>
-            ))
-          ) : (
-            <h2>Loading...</h2>
-          )}
+          <h5>{user.attributes?.email}</h5>
+          <ul className="row">
+            {listing ? (
+              listing.map(l => (
+                <li className="col-12 col-sm-6 col-md-4 col-lg-3 mt-4" key={uuid_v4()}>
+                  {/* CARD */}
+                  <div className="listing-profile card h-100">
+                    {/* PHOTO */}
+                    <ul className="photos">
+                      {l.images_url.map((image, index) => (
+                        <li key={index}>
+                          <img src={`${process.env.REACT_APP_API_URL}${image}`} 
+                                alt="" 
+                                className="card-img-top"/>
+                        </li>
+                      ))}
+                    </ul>
+                    {/* CONTENT */}
+                    <div className="card-body">
+                      <p>{l.title}</p>
+                      <p>{l.description}</p>
+                      <p>{l.address}</p>
+                      <p>{l.location}</p>
+                      <Link to={`/update_real_estate/${l.id}`} 
+                            className="btn btn-primary">Update</Link>
+                    </div>
+                  </div>
+                  {/* CARD : end */}
+                </li>
+              ))
+            ) : (
+              <h2>Loading...</h2>
+            )}
+          </ul>
         </div>
       ) : (
         <h1>Profile</h1>
